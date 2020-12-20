046f2e42c7a1600d0819d23eadd942911524c2c1
Timo Fischer <timo.fischer1308@gmail.com>
2020-12-20 13:32:33 +0100

    added Sensor Configuration options for ColorSensor and TouchSensor available ColorSensor positions are: front (default), back, left and right available ColorSensor alignments are: down (default) and horizontal available TouchSensor positions are: front (default) and back

--- blocks/robBrick.js
+++ blocks/robBrick.js
@@ -388,9 +388,22 @@ Blockly.Blocks['robBrick_colour'] = {
      */
     init : function() {
         this.setColour(Blockly.CAT_SENSOR_RGB);
-        this.appendDummyInput().appendField(Blockly.Msg.SENSOR_COLOUR);
-        this.setOutput(true, 'Sensor');
         this.setTooltip(Blockly.Msg.COLOUR_TOOLTIP);
+        this.appendDummyInput().appendField(Blockly.Msg.SENSOR_COLOUR);
+        if (this.workspace.device !== 'botnroll') { // disable dropdown selection for only Bot'n Roll
+            var sensorPosition = new Blockly.FieldDropdown([ 
+                [ Blockly.Msg.SLOT_FRONT, 'FRONT' ], 
+                [ Blockly.Msg.SLOT_LEFT, 'LEFT' ], 
+                [ Blockly.Msg.SLOT_RIGHT, 'RIGHT' ], 
+                [ Blockly.Msg.SLOT_BACK, 'BACK' ] ]);
+            var sensorAlignment = new Blockly.FieldDropdown([ 
+                [ Blockly.Msg.ALIGNMENT_DOWN, 'DOWN' ], 
+                [ Blockly.Msg.ALIGNMENT_HORIZONTAL, 'HORIZONTAL' ] ]);
+            this.appendDummyInput().appendField(Blockly.Msg.SENSOR_POSITION).appendField(sensorPosition, 'SENSOR_POSITION').setAlign(Blockly.ALIGN_RIGHT);
+            this.appendDummyInput().appendField(Blockly.Msg.SENSOR_ALIGNMENT).appendField(sensorAlignment, 'SENSOR_ALIGNMENT').setAlign(Blockly.ALIGN_RIGHT);
+            this.setTooltip(Blockly.Msg.COLOUR_TOOLTIP + " " + Blockly.Msg.TOOLTIP_SIMULATION_ONLY);
+        }
+        this.setOutput(true, 'Sensor');
     }
 };
 
@@ -490,9 +503,16 @@ Blockly.Blocks['robBrick_touch'] = {
 
     init : function() {
         this.setColour(Blockly.CAT_SENSOR_RGB);
-        this.appendDummyInput().appendField(Blockly.Msg.SENSOR_TOUCH);
-        this.setOutput(true, 'Sensor');
         this.setTooltip(Blockly.Msg.TOUCH_TOOLTIP);
+        this.appendDummyInput().appendField(Blockly.Msg.SENSOR_TOUCH);
+        if (this.workspace.device !== 'botnroll') { // disable dropdown selection for only Bot'n Roll
+            var sensorPosition = new Blockly.FieldDropdown([
+                [ Blockly.Msg.SLOT_FRONT, 'FRONT' ],
+                [ Blockly.Msg.SLOT_BACK, 'BACK' ] ]);
+            this.appendDummyInput().appendField(Blockly.Msg.SENSOR_POSITION).appendField(sensorPosition, 'SENSOR_POSITION').setAlign(Blockly.ALIGN_RIGHT);
+            this.setTooltip(Blockly.Msg.TOUCH_TOOLTIP + " " + Blockly.Msg.TOOLTIP_SIMULATION_ONLY);
+        }
+        this.setOutput(true, 'Sensor');
     }
 };
 
